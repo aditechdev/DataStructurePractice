40db5c91d4d29a28620d614cc1476bf062111d68
Aditya Anand <contactaditechdev@gmail.com>
2025-10-04 22:38:01 +0530

    Observation for the palendrom updated

--- src/begineer/pattern_problems.java
+++ src/begineer/pattern_problems.java
@@ -4,7 +4,9 @@ public class pattern_problems {
 
     public static void main(String[] args) {
         int n = 5;
-        half_number_pyramid(n);
+        halfNumberPyramid(n);
+        System.out.println();
+        palindromicPattern(n);
     }
 
     // Half Number Pyramid
@@ -13,7 +15,7 @@ public class pattern_problems {
     // 3 4 5
     // 4 5 6 7
     // 5 6 7 8 9
-    static void half_number_pyramid(int n) {
+    static void halfNumberPyramid(int n) {
         System.out.println("Half Number Pyramid:");
 
         for (int i = 1; i <= n; i++) {
@@ -24,5 +26,43 @@ public class pattern_problems {
             }
             System.out.println();
         }
+    }
+
+    //    1
+    //   232
+    //  34543
+    // 4567654
+    //567898765
+    // Observation:
+    // Every row start with value equal to row number.
+    // We have to print n rows.
+    // Every row has N-1 spaces. i number in increasing order followed by i-1 number
+    // in decreasing order.
+    // Use AP formula to solve all patern problem
+    // a+(i-1)*d
+    // Here space are like  4,3,2,1,0
+    // 4 + (i-1)*(-1) = 4 -i +1 = 5 -i = N-i
+    
+    // 
+    static void palindromicPattern(int n) {
+        System.out.println("Palindromic Patter:");
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + j - 1);
+            }
+            // decreasing numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(i + j - 1);
+            }
+
+            System.out.println();
+        }
+
     }
 }
