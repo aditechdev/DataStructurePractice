4d446326579ade4fc04aac5808d851cbdf91d26a
Aditya Anand <contactaditechdev@gmail.com>
2025-10-04 23:00:40 +0530

    halfDiamondProblem solved

--- src/begineer/pattern_problems.java
+++ src/begineer/pattern_problems.java
@@ -7,6 +7,7 @@ public class pattern_problems {
         halfNumberPyramid(n);
         System.out.println();
         palindromicPattern(n);
+        halfDiamondPattern(n);
     }
 
     // Half Number Pyramid
@@ -42,7 +43,6 @@ public class pattern_problems {
     // a+(i-1)*d
     // Here space are like  4,3,2,1,0
     // 4 + (i-1)*(-1) = 4 -i +1 = 5 -i = N-i
-    
     // 
     static void palindromicPattern(int n) {
         System.out.println("Palindromic Patter:");
@@ -64,5 +64,32 @@ public class pattern_problems {
             System.out.println();
         }
 
+    }
+
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //*********
+
+    static void halfDiamondPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            // Print Space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 1,3,5,7
+            // a = 1
+            // d = a[2] - a[1] = 2
+            // i = ithTerm
+            // a+(i-1)*d
+            // 1 + (i-1) * 2 
+            // 2i -1
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(("*"));
+            }
+            System.out.println();
+        }
     }
 }
