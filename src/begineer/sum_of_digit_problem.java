6c76ddcdb9c62ab3ea73fbf4478204fe722855e5
Aditya Anand <contactaditechdev@gmail.com>
2025-10-05 00:37:28 +0530

    sum of digit problem slved

---
+++ src/begineer/sum_of_digit_problem.java
@@ -0,0 +1,22 @@
+package begineer;
+
+public class sum_of_digit_problem {
+
+    public static void main(String[] args) {
+        sumOfDigitProblem(35435);
+    }
+
+    static void sumOfDigitProblem(int number) {
+
+        int sume = 0;
+        int newNumber = number;
+
+        while (newNumber != 0) {
+            sume = sume + (newNumber % 10);
+            newNumber = newNumber / 10;
+            System.out.println("Sume: " + sume);
+            System.out.println("NewNumber = " + newNumber);
+        }
+
+    }
+}
