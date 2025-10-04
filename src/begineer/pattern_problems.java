a7bce8d181abb20b6db4a5192a52a533161f3ab0
Aditya Anand <contactaditechdev@gmail.com>
2025-10-04 21:00:01 +0530

    Refactor pattern_problems class and enhance half_number_pyramid method with improved formatting

--- src/begineer/pattern_problems.java
+++ src/begineer/pattern_problems.java
@@ -1,8 +1,27 @@
 package begineer;
 
-public class pattern_problems   {
+public class pattern_problems {
 
     public static void main(String[] args) {
+        half_number_pyramid(5);
+    }
 
+    // Half Number Pyramid
+    // 1
+    // 2 3
+    // 3 4 5
+    // 4 5 6 7
+    // 5 6 7 8 9
+    static void half_number_pyramid(int n) {
+        System.out.println("Half Number Pyramid:");
+
+        for (int i = 1; i <= n; i++) {
+            int count = i;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
     }
 }
